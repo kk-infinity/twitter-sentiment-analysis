709a4ecf965c742a1c3b6ccea30d6d87f023ee1c
keerthikkal764 <52378795+keerthikkal764@users.noreply.github.com>
2019-09-20 16:14:54 -0400

    Code to pull tweets from twitter API
    - Pulls tweets from Twitter API
    - Pre-processing of tweets
    - Cleaning of tweets

---
+++ config.py
@@ -0,0 +1,6 @@
+keys = {
+    "consumer_api_key": "<Consume API Key>",
+    "consumer_api_secret_key": "<Consumer API Secret Key>",
+    "access_token": "<Access Token>",
+    "access_token_secret": "<Access Token Secret>"
+}
